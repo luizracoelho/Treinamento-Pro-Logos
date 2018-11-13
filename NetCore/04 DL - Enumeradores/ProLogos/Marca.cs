a3ec6cd33b9f16847f1b812f96fd34c29fd8ba42
Luiz R. A. Coelho <luizracoelho@gmail.com>
2018-11-13 17:28:35 -0200

    Criação da aula 4

---
+++ NetCore/04 DL - Enumeradores/ProLogos/Marca.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProLogos
+{
+    public class Marca
+    {
+        #region Properties
+        private string _nome;
+        public string Nome { get => _nome; set => SetNome(value); }
+
+        public string Segmento { get; set; }
+        public DateTime DataCadastro { get; private set; }
+        public virtual IList<Produto> Produtos { get; protected set; }
+        #endregion
+
+        #region Constructors
+        protected Marca() { }
+
+        public Marca(string nome, string segmento = null)
+        {
+            Nome = nome;
+            Segmento = segmento;
+            DataCadastro = DateTime.Now;
+        }
+        #endregion
+
+        #region Methods
+        private void SetNome(string nome)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentNullException(nameof(Nome));
+            else if (nome.Length > 30)
+                throw new ArgumentOutOfRangeException(nameof(Nome), "O campo nome deve possuir até 30 caracteres");
+
+            _nome = nome;
+        }
+        #endregion
+    }
+}
